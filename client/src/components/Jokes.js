5ad255c0f6e559a7f88a1ebe087c31be6813220c
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-15 11:00:36 -0700

    joke cards added and styled, stretch done

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -22,15 +22,50 @@ class Jokes extends React.Component {
 
     render() { 
         return ( 
-            <div>
+            <div className='container'>
                 <Link to="/">
                     <button className="home-button">
                         Home
                     </button>
                 </Link>
-                {this.state.jokes.map(joke => <div key={joke._id}>{joke.setup}</div>)}
+                <div className='row justify-content-sm-center'>
+                {this.state.jokes.map(joke => {
+                    return (
+                        // <div key={joke._id}>{joke.setup}</div>
+                        <div key={joke._id} className="col-sm-4 friend-card mt-sm-4">
+                            <div className="image-flip">
+                                <div className="mainflip">
+                                    <div className="frontside">
+                                        <div className="card">
+                                            <div className="card-body text-center">
+                                                <h4 className="card-title">
+                                                    {joke.type}
+                                                </h4>
+                                                <p className="card-title">
+                                                    {joke.setup}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="backside">
+                                        <div className="card">
+                                            <div className="card-body text-center">
+                                                <h4 className="card-title">
+                                                   {joke.punchline}
+                                                </h4>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+
+                })}    
+                </div>
             </div>
-         )
+         ) 
     }
 }
  
