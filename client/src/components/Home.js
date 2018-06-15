d3bee101f9c99dfebfb79671285c1a2d1a624106
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-15 10:28:18 -0700

    signout button added to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Home = props => {
-    return (
-        <div className="App" >
-            <Link to="/register" > Sign up </Link >
-            <Link to="/login" > Log In </Link >
-        </div >
-    );
+class Home extends React.Component {
+    state = {
+        users: [],
+        username: ''
+    }
+
+    componentWillMount() {
+        let username = localStorage.getItem('username');
+        username = username ? username : '';
+        this.setState({ username: username })
+    }
+
+    signout = () => {
+        if (localStorage.getItem('token')) {
+            localStorage.removeItem('token');
+            this.props.history.push('/login');
+        }
+    };
+
+    render() {
+        return (
+            <div className="App" >
+                {!localStorage.getItem('token') && (
+                    <Link to="/register" > Sign up </Link >
+                )}
+                {!localStorage.getItem('token') && (
+                    <Link to="/login" > Log In </Link >                
+                )}
+                <h3>Welcome {this.state.username}! Wanna see some jokes?</h3>
+                <Link to="/jokes" > Jokes </Link >                
+                <button onClick={this.signout} className="home-button">
+                    Sign out
+                </button>
+            </div >
+        );
+    }
 };
 
 export default Home;
